a4845a194da4d363cfa8dff96f0a48456eb6d3d2
Kazakov Andrei <Deimos678@gmail.com>
2023-02-20 11:14:28 +0300

    Coroutine scope

--- app/src/main/java/com/example/shoppinglist/data/AppDatabase.kt
+++ app/src/main/java/com/example/shoppinglist/data/AppDatabase.kt
@@ -6,11 +6,12 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun shopListDao(): ShopListDao
 
     companion object {
+
         private var INSTANCE: AppDatabase? = null
         private val LOCK = Any()
         private const val DB_NAME = "shop_item.db"
